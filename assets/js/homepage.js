b66eac677d800287713618b72465cbf5b418a78f
MinhHa Pham <minhha@MinhHas-MacBook-Air.local>
2021-07-24 17:52:05 -0700

    add date and weather forecast

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,21 @@
 // get city search term and use .value to store its value
+
+function pastCity(city) {
+  localStorage.setItem("city-name", city);
+  
+  var cityStorage = document.createElement ("button");
+  cityStorage.setAttribute("class", "pastCity form-control");
+  cityStorage.setAttribute("id",city);
+  var pastCityResults = document.querySelector("#search");
+  pastCityResults.appendChild(cityStorage);
+  cityStorage.innerHTML=city;
+};
+
 function myFunction () {
   var searchTerm = document.querySelector("#searchTerm").value; 
   event.preventDefault();
   console.log(searchTerm); 
+  pastCity(searchTerm);
 
   // Make a `fetch` request concatenating that search term to the query URL for current weather
   fetch (
@@ -18,13 +31,13 @@ function myFunction () {
   .then(function(response) {
     console.log(response.data);
   
-  // use jQuery to display current weather using elements from the response
-    $("#city-name").html(response.name);
+  // use jQuery to display current date and weather using elements from the response
+    var currentDate = moment().format("M/D/YYYY").toString();
+    $("#city-name").html(response.name + " " + currentDate); 
     $("#temp").html("Temp: " + response.main.temp + " °F");
     $("#speed").html("Wind: " + response.wind.speed + " mph");
     $("#humidity").html("Humidity: " + response.main.humidity + "%");
-  
-        // $("#index").html("UV Index: " + response.current.uvi);                         CAN'T FIND UV INDEX IN THE CURRENT API
+    console.log(response.weather[0].icon);
 
     // Make a `fetch` request concatenating that search term to the query URL for weather forecast
     fetch (
@@ -40,18 +53,40 @@ function myFunction () {
     .then(function(response) {
       console.log(response.data);
       
-      
+      /* use jQuery to bring weather data from the API to the 5-day forecast cards */
 
-      /* adding variables for moment .js, and api to display each day of 5-day forecast date, icon, temp, and humidity */
+      /* DAY 1 FORECAST */
+      $("#dayOne").html(moment().add(1, "days").format("M/D/YYYY").toString());
+      $("#dayOneTemp").html("Temp: " + response.list[2].main.temp + " °F");
+      $("#dayOneSpeed").html("Wind: " + response.list[2].wind.speed + " mph");
+      $("#dayOneHumidity").html("Humidity: " + response.list[2].main.humidity + "%");
 
+      /* DAY 2 FORECAST */
+      $("#dayTwo").html(moment().add(2, "days").format("M/D/YYYY").toString());
+      $("#dayTwoTemp").html("Temp: " + response.list[8].main.temp + " °F");
+      $("#dayTwoSpeed").html("Wind: " + response.list[10].wind.speed + " mph");
+      $("#dayTwoHumidity").html("Humidity: " + response.list[10].main.humidity + "%");
 
+      /* DAY 3 FORECAST */
+      $("#dayThree").html(moment().add(3, "days").format("M/D/YYYY").toString());
+      $("#dayThreeTemp").html("Temp: " + response.list[16].main.temp + " °F");
+      $("#dayThreeSpeed").html("Wind: " + response.list[16].wind.speed + " mph");
+      $("#dayThreeHumidity").html("Humidity: " + response.list[16].main.humidity + "%");
+
+      /* DAY 4 FORECAST */
+      $("#dayFour").html(moment().add(4, "days").format("M/D/YYYY").toString());
+      $("#dayFourTemp").html("Temp: " + response.list[24].main.temp + " °F");
+      $("#dayFourSpeed").html("Wind: " + response.list[24].wind.speed + " mph");
+      $("#dayFourHumidity").html("Humidity: " + response.list[24].main.humidity + "%");
+
+      /* DAY 5 FORECAST */
+      $("#dayFive").html(moment().add(5, "days").format("M/D/YYYY").toString());
+      $("#dayFiveTemp").html("Temp: " + response.list[32].main.temp + " °F");
+      $("#dayFiveSpeed").html("Wind: " + response.list[32].wind.speed + " mph");
+      $("#dayFiveHumidity").html("Humidity: " + response.list[32].main.humidity + "%");
 
     })
-
-  
-
   })  
-
 }
   
 
